134b2213ca7ebaedd448cb448b93c43a28cf217c
chancegiguiere <34294213+chancegiguiere@users.noreply.github.com>
2021-05-31 18:22:35 -0700

    Finished Basic blog functionality
    Posting, editing, deleting are all supported.
    
    Want to implement:
    Viewing all posts by a single author
    Searching posts by title
    Multiple page support to accomodate for many blog articles in database (10 per page or something like that)
    
    Stretch Goals:
    Some form of authentication before a user can edit or delete a post (especially if it's another user's post)

--- models/article.js
+++ models/article.js
@@ -1,4 +1,10 @@
 const mongoose = require('mongoose')
+const marked = require('marked')
+const slugify = require('slugify')
+const createDomPurify = require('dompurify')
+const {JSDOM} = require('jsdom')
+const dompurify = createDomPurify(new JSDOM().window)
+
 const articleSchema = new mongoose.Schema({
     title: {
         type: String,
@@ -19,8 +25,29 @@ const articleSchema = new mongoose.Schema({
     author:{
         type: String,
         required: true
+    },
+
+    slug:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    sanitizedHtml:{
+        type: String,
+        required: true
+    }
+})
+
+articleSchema.pre('validate', function(next){
+    if(this.title){
+        this.slug = slugify(this.title, {lower: true, strict: true, } )
     }
 
+    if(this.markdown){
+        this.sanitizedHtml = dompurify.sanitize(marked(this.markdown))
+    }
+    
+    next()
 })
 
 module.exports = mongoose.model('Article', articleSchema)
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const articleRouter = require('./routes/articles')
 const mongoose = require('mongoose')
 const Article = require('./models/article')
+const methodOverride = require('method-override')
 
 
 mongoose.connect('mongodb://localhost/blog', {
@@ -14,6 +15,7 @@ mongoose.connect('mongodb://localhost/blog', {
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended:false}))
+app.use(methodOverride('_method'))
 
 
 app.get('/', async (req, res)=>{
