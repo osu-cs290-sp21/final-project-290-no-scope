ab065afcdd834fd78f9340ddd9c4823f6f107a6e
Andrew Kassab <andrewkassab7@gmail.com>
2021-06-03 16:09:19 -0700

    Deleting Mongo

--- server.js
+++ server.js
@@ -1,33 +1,8 @@
 const express = require('express')
 const server = express()
 var exphbs = require('express-handlebars')
-var mongoose = require('mongoose'); //required in order to use MongoDB
 const Article = require('./models/article');
 var port = process.env.PORT||3000;
-
-mongoose.connect('mongodb://localhost:27017/blog', {
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
-    
-})
-
-/*(var MongoClient = require('mongodb').MongoClient;
-
-var mongoHost = process.env.MONGO_HOST;
-var mongoPort = process.env.MONGO_PORT || 27017
-var mongoUser = process.env.MONGO_USER;
-var mongoPassword = process.env.MONGO_PASSWORD;
-var mongoDBName = process.env.MONGO_DB_NAME;
-
-var mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword + '@' + mongoHost + ':' + mongoPort + '/' + mongoDBName;
-
-MongoClient.connect({
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
-})*/
-
-
-
-// Set up MongoDB
-
 
 
 // Serve static files
@@ -42,7 +17,7 @@ server.get("/", function(req, res, next){
         res.status(200).render("homePage",{articles:article});
     })
 
-    
+
     return;
 });
 
