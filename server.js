a128386296ddd7d05db68ee63e7f72c990328b7d
chancegiguiere <34294213+chancegiguiere@users.noreply.github.com>
2021-06-06 19:23:51 -0700

    doing cool stuff
    testing promises

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express')
 const server = express()
 var exphbs = require('express-handlebars')
 var fs = require('fs');
-var port = process.env.PORT||3000;
+var port = process.env.PORT || 3000;
 
 const Article = require('./models/article');
 
 
 // Serve static files
-server.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
+server.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 server.set('view engine', 'hbs')
 server.use(express.static('public'))
 
@@ -20,78 +20,87 @@ const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('blog.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     console.error(err.message);
-  }else{
+  } else {
     console.log('Connected to the database.');
   }
 });
 
+var postArray = new Array();
+var iterator = 0;
+
 //Here I am selecting specific elements from the table I created within the Database
 //And checking for any errores that occure if not I am printing the different elements of the table
-db.serialize(() => {
-  db.each(`SELECT id as id,
-                  title as title,
-                  author as author,
-                  description as description,
-                  content as content,
-                  date as date
-           FROM blog_post`, (err, row) => {
-    if (err) {
-      console.error(err.message);
-      console.log("Cannot find columns")
-    }
-    console.log("------------------------------------------------")
-    console.log(row.id);
-    console.log(row.author)
-    console.log(row.description)
-    console.log(row.content)
-    console.log(row.date)
-    console.log("------------------------------------------------")
+let dbResponse = async () => {
 
-  });
-});
 
+  let response = await new Promise((resolve, reject) => {
+    var query = `SELECT id as id,
+    title as title,
+    author as author,
+    description as description,
+    content as content,
+    date as date
+    FROM blog_post`
+    
+    db.each(query, (err, row) => {
+      if (err) {
+        console.error(err.message);
+        console.log("Cannot find columns")
+        reject(new Error("Connection gone bad"))
+      }
+
+      else {
+        
+        resolve(row)
+      }
+    })
+  }).catch(err => console.log(err));
+  var resp = JSON.parse(JSON.stringify(response))
+  return resp;
+}
 
 //-----------------------------------------------------------------------------
 
 
-server.get("/", function(req, res, next){
-    Article.find({}).lean()
-        .exec(function(err, article){
-        res.status(200).render("homePage",{articles:article});
-    })
 
-    return;
+server.get("/", async (req, res) => {
+  var resp = await dbResponse()
+  //console.log(JSON.stringify(resp));
+  console.log(resp)
+  res.status(200).render("homePage", { articles: resp });
+  console.log("GET request recieved from /")
 });
 
-server.get("/new", function(req, res, next){
-    res.status(200).render("newPost",{
-    //serve blog posts from mongodb or other database (figure out how)
-    })
-    return;
+server.get("/new", function (req, res, next) {
+  res.status(200).render("newPost", {
+  })
+  console.log("GET request recieved from /new")
+
 });
 
-server.get("/:id", function(req, res, next){
-    var article =  Article.findOne({id:req.params.id}).lean();
-    if(article == null){res.redirect('404')}
+server.get("/:id", function (req, res, next) {
+  var article = Article.findOne({ id: req.params.id }).lean();
+  if (article == null) { res.redirect('404') }
 
-    res.status(200).render('singlePost', {article:Article})
+  res.status(200).render('singlePost', { article: Article })
+  console.log("GET request recieved from /:id")
 })
 
 
 
-server.get("/edit/:id", function(req, res, next){
-    res.status(200).render("editPost",{
+server.get("/edit/:id", function (req, res, next) {
+  res.status(200).render("editPost", {
     //serve blog posts from mongodb or other database (figure out how)
-    })
-    return;
+  })
+  console.log("GET request recieved from /edit/:id")
 });
 
 
-server.get("*", function(req, res){
-    res.status(404).render("404",{
+server.get("*", function (req, res) {
+  res.status(404).render("404", {
     //404 Comment not found
-    })
-    return;
+  })
+  return;
 });
 
 // server.get('/blog', (req, res) => {
@@ -99,10 +108,9 @@ server.get("*", function(req, res){
 // })
 
 // Exports our server for use elsewhere
-module.exports = server
 
 
-
-server.listen(port, function () {
-  console.log("== Server is listening on port", port);
+server.listen(port, () => {
+  console.log("== Server is listening on port", port)
 });
+module.exports.server = server
