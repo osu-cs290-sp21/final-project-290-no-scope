877cb1367f50bf48b24381e4fa78596f2576019c
dominichsiao <72414659+dominichsiao@users.noreply.github.com>
2021-06-07 14:32:10 -0700

    Layed basic framework for index.js

--- public/index.js
+++ public/index.js
@@ -1,12 +1,37 @@
-search = document.getElementById("navbar-search-input").value
-title = document.getElementById("title").value
-author = document.getElementById("author").value
-description = document.getElementById("navbar-search-input").value
-content = document.getElementById("navbar-search-input").value
+var searchInput = document.getElementById("navbar-search-input").value
+var title = document.getElementById("title").value
+var author = document.getElementById("author").value
+var description = document.getElementById("navbar-search-input").value
+var content = document.getElementById("navbar-search-input").value
+var search = document.getElementById("navbar-search-input")
 
 
 submitButton = document.getElementsByClassName("submit-button")[0]
 cancelButton = document.getElementsByClassName("cancel-button")[0]
 
+function insertBlogPost() {
+    console.log("Insert new Blog Posts")
+}
 
+function createBlogPost() {
+    console.log("Create a new Blog Post")
+}
 
+function update(){
+    console.log("This will be the search")
+}
+
+search.addEventListener('input', function(){
+    update()
+})
+
+submitButton.addEventListener('click', function(){
+    createBlogPost()
+})
+
+cancelButton.addEventListener('click', function(){
+    author = ""
+    title = ""
+    description = ""
+    content = ""
+})
