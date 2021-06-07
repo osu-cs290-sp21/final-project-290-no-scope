7f8f0f02117311e0e4a3fc9e9d192dc8b75d6521
dominichsiao <72414659+dominichsiao@users.noreply.github.com>
2021-06-07 14:19:15 -0700

    Making client-side JS and adding some variables

---
+++ public/index.js
@@ -0,0 +1,12 @@
+search = document.getElementById("navbar-search-input").value
+title = document.getElementById("title").value
+author = document.getElementById("author").value
+description = document.getElementById("navbar-search-input").value
+content = document.getElementById("navbar-search-input").value
+
+
+submitButton = document.getElementsByClassName("submit-button")[0]
+cancelButton = document.getElementsByClassName("cancel-button")[0]
+
+
+
