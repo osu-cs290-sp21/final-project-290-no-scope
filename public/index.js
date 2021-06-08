c7af5c71d2e9729489678845ca153aff4cb7ebdd
dominichsiao <72414659+dominichsiao@users.noreply.github.com>
2021-06-07 20:23:44 -0700

    Tried first search function

--- public/index.js
+++ public/index.js
@@ -5,6 +5,7 @@ var description = document.getElementById("navbar-search-input").value
 var content = document.getElementById("navbar-search-input").value
 var search = document.getElementById("navbar-search-input")
 
+var blogs = document.querySelectorAll(".article-preview")
 
 submitButton = document.getElementsByClassName("submit-button")[0]
 cancelButton = document.getElementsByClassName("cancel-button")[0]
@@ -19,6 +20,15 @@ function createBlogPost() {
 
 function update(){
     console.log("This will be the search")
+    var blogContent = document.getElementsByClassName("article-preview")[0]
+    for(ii = 0; i < blogs.length; i++){
+        if(blogs[i].textContent.toLowerCase().includes(search.value.toLowerCase()) == false){
+            blogs[i].classList.add("hidden")
+        }
+        else{
+            blogs[i].classList.remove("hidden")
+        }
+    }
 }
 
 search.addEventListener('input', function(){
