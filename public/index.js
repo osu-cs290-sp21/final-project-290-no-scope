54e96417239c804b1c58e8079240946d9bbcf8ee
dominichsiao <72414659+dominichsiao@users.noreply.github.com>
2021-06-07 21:01:38 -0700

    Search should be done

--- public/index.js
+++ public/index.js
@@ -1,14 +1,18 @@
-var searchInput = document.getElementById("navbar-search-input").value
-var title = document.getElementById("title").value
-var author = document.getElementById("author").value
-var description = document.getElementById("navbar-search-input").value
-var content = document.getElementById("navbar-search-input").value
+
+var searchInput = document.getElementById("navbar-search-input")
+var title = document.getElementById("title")
+var author = document.getElementById("author")
+var description = document.getElementsByClassName("description").value
+//var content = document.getElementById("navbar-search-input").value
 var search = document.getElementById("navbar-search-input")
+
 
 var blogs = document.querySelectorAll(".article-preview")
 
 submitButton = document.getElementsByClassName("submit-button")[0]
 cancelButton = document.getElementsByClassName("cancel-button")[0]
+
+
 
 function insertBlogPost() {
     console.log("Insert new Blog Posts")
@@ -19,9 +23,9 @@ function createBlogPost() {
 }
 
 function update(){
-    console.log("This will be the search")
+    //console.log("This will be the search")
     var blogContent = document.getElementsByClassName("article-preview")[0]
-    for(ii = 0; i < blogs.length; i++){
+    for(i = 0; i < blogs.length; i++){
         if(blogs[i].textContent.toLowerCase().includes(search.value.toLowerCase()) == false){
             blogs[i].classList.add("hidden")
         }
@@ -31,17 +35,23 @@ function update(){
     }
 }
 
-search.addEventListener('input', function(){
+if(search){
+search.addEventListener('keyup', function(){
     update()
 })
+}
 
+if(submitButton){
 submitButton.addEventListener('click', function(){
     createBlogPost()
 })
+}
 
+if(cancelButton){
 cancelButton.addEventListener('click', function(){
     author = ""
     title = ""
     description = ""
     content = ""
 })
+}
