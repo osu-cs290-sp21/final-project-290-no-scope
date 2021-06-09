f2a88fab5205a09bd20574335c56fbba00993175
chancegiguiere <34294213+chancegiguiere@users.noreply.github.com>
2021-06-09 12:37:23 -0700

    Add files via upload

---
+++ models/article.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var articleSchema = new mongoose.Schema({
+
+title:
+   {type: String, required: true},
+author:
+   {type: String, required: true},
+createdAt:
+   {type: Date, default: Date.now},
+description:
+   {type: String},
+content:
+   {type: String, required: true}
+});
+module.exports = mongoose.model('Article', articleSchema);
