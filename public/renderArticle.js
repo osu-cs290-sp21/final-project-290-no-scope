8d8dad40499a56860ded7590d6bbd34609ebc006
Andrew Kassab <andrewkassab7@gmail.com>
2021-06-09 00:40:43 -0700

    Got time stanp to print to server and save to database

--- public/renderArticle.js
+++ public/renderArticle.js
@@ -8,12 +8,12 @@ templates['article.hbs'] = template({"compiler":[8,">= 4.3.0"],"main":function(c
         return undefined
     };
 
-  return "<article class=\"article\">\r\n  \r\n  <div class=\"article-content\">\r\n    <h1 class=\"article-title\">\r\n        "
+  return "<article class=\"article\">\n  \n  <div class=\"article-content\">\n    <h1 class=\"article-title\">\n        "
     + alias4(((helper = (helper = lookupProperty(helpers,"title") || (depth0 != null ? lookupProperty(depth0,"title") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data,"loc":{"start":{"line":5,"column":8},"end":{"line":5,"column":17}}}) : helper)))
-    + "\r\n    </h1>\r\n\r\n    <p class=\"article-author\">\r\n      <a href=\"#\">\r\n          "
+    + "\n    </h1>\n\n    <p class=\"article-author\">\n      <a href=\"#\">\n          "
     + alias4(((helper = (helper = lookupProperty(helpers,"author") || (depth0 != null ? lookupProperty(depth0,"author") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"author","hash":{},"data":data,"loc":{"start":{"line":10,"column":10},"end":{"line":10,"column":20}}}) : helper)))
-    + "</a>\r\n    </p>\r\n\r\n    <p class=\"article-markdown\">\r\n      "
+    + "</a>\n    </p>\n\n    <p class=\"article-markdown\">\n      "
     + alias4(((helper = (helper = lookupProperty(helpers,"markdown") || (depth0 != null ? lookupProperty(depth0,"markdown") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"markdown","hash":{},"data":data,"loc":{"start":{"line":14,"column":6},"end":{"line":14,"column":18}}}) : helper)))
-    + "\r\n    </p>\r\n    \r\n    \r\n  </div>\r\n</article>\r\n<!-- Need to pull post data from a database, not a JSON like with assignment 5.\r\n    This file represents the article previews that will populate the home page -->";
+    + "\n    </p>\n    \n    \n  </div>\n</article>\n<!-- Need to pull post data from a database, not a JSON like with assignment 5.\n    This file represents the article previews that will populate the home page -->";
 },"useData":true});
 })();
